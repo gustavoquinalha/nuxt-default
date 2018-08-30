8188d4dc2c6c02fc497206abf4094e8aed9d2631
gustavoquinalha <gustavo_quinalha@hotmail.com>
2018-08-30 00:45:47 +0000

    Create Styleguide

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,5 +9,19 @@ module.exports = {
   srcDir: 'client/',
   build: {
     watch: [ 'server/' ]
-  }
+  },
+  css: [
+    '@/assets/css/main.scss'
+  ],
+  head: {
+    script: [
+    ],
+    link: [
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Roboto' },
+      { rel: 'stylesheet', href: 'https://cdn.jsdelivr.net/npm/the-grid-flexbox@2.0.1/css/the-grid.min.css' }
+    ]
+  },
+  modules: [
+    ['nuxt-sass-resources-loader', '@/assets/css/base/_variables.scss']
+  ],
 }
--- src/server/index.js
+++ src/server/index.js
@@ -1,20 +1,20 @@
 import express from 'express'
-import bodyParser from 'body-parser'
-
-import users from './../routes/users'
+// import bodyParser from 'body-parser'
+//
+// import users from './../routes/users'
 
 const app = express()
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({
-  extended: false
-}))
-
-app.get('/api/v1/users', users.all)
-app.post('/api/v1/users', users.create)
-app.delete('/api/v1/users/:id', users.remove)
-app.put('/api/v1/users/:id', users.update)
-
-app.post('/api/v1/login', users.login)
+// app.use(bodyParser.json())
+// app.use(bodyParser.urlencoded({
+//   extended: false
+// }))
+//
+// app.get('/api/v1/users', users.all)
+// app.post('/api/v1/users', users.create)
+// app.delete('/api/v1/users/:id', users.remove)
+// app.put('/api/v1/users/:id', users.update)
+//
+// app.post('/api/v1/login', users.login)
 
 export default app
